1ebd02e90a24a002110b7948559786c36dbccbb1
Chandrasekhar0583 <chandumajji4444@gmail.com>
2025-06-13 10:42:55 +0530

    vercel .js

--- teja-front-end/vite.config.js
+++ teja-front-end/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  base: './', // ✅ REQUIRED to fix white screen + MIME issues on Vercel
+
   build: {
-    chunkSizeWarningLimit: 500000, // Default is 500 kB
+    chunkSizeWarningLimit: 500000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return 'vendor'; // Moves all node_modules imports into a separate chunk
+            return 'vendor';
           }
         }
       }
