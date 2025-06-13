40f375e32f4b6a11ec73019a338427afd1c5ea96
Chandrasekhar0583 <chandumajji4444@gmail.com>
2025-06-13 10:56:58 +0530

    vercel4

--- teja-front-end/vite.config.js
+++ teja-front-end/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/', // Use absolute paths for Vercel
   build: {
     chunkSizeWarningLimit: 500000,
     rollupOptions: {
