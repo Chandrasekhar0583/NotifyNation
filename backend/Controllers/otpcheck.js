bde6f21b64c39646e4251d67fd6bc4a114b3b81a
Chandrasekhar0583 <chandumajji4444@gmail.com>
2025-09-13 15:05:14 +0530

    otp2

--- backend/Controllers/otpcheck.js
+++ backend/Controllers/otpcheck.js
@@ -41,19 +41,19 @@ const SendOtp = async(req,res)=>{
     </p>
   </div>`;
 
-const mailOptions = {
-  from: process.env.SMTP_MAIL,
-  to: email,
-  subject: "Notify Nation - Verification OTP",
-  html: otpHTML
-};
+// const mailOptions = {
+//   from: process.env.SMTP_MAIL,
+//   to: email,
+//   subject: "Notify Nation - Verification OTP",
+//   html: otpHTML
+// };
 
-  //  const mailOptions = {
-  //   from: process.env.SMTP_MAIL,
-  //   to: email,
-  //   subject: "OTP",
-  //   text: `${num}`
-  // };
+   const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: email,
+    subject: "OTP",
+    text: `${num}`
+  };
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
       return res.status(500).json({ error: "Failed to send email" });
